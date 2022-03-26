e939a26f7f2ad0b900473d7d9d3d562609e2b1b2
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-03-26 11:46:11 -0300

    Inserindo dados no MySQL

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.post('/books/insertBook', (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+
+  conn.query(sql, (err) => {
+    if (err) console.log(err);
+
+    res.redirect('/');
+  });
 });
 
 const conn = mysql.createConnection({
